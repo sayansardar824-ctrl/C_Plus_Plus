6c0cdafaf6ae88f90c049505e2dfb065c87756fd
sayansardar824-ctrl <sayansardar824@gmail.com>
2026-08-17 13:52:47 +0530

    Add files via upload

---
+++ Natural Square Sum.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, s = 0;
+    printf("How many natural number to be added ");
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        s = s + (2 * i - 1);
+    }
+    printf("The sum is= %d", s);
+    return 0;
+}

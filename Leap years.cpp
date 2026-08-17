8fc4ed92a84a57e3a2023b3dcf94981c079ec286
sayansardar824-ctrl <sayansardar824@gmail.com>
2026-08-17 13:36:44 +0530

    Update Leap years.cpp

--- Leap years.cpp
+++ Leap years.cpp
@@ -4,14 +4,14 @@ int main()
 	int year;
 	printf(" Enter the year :");
 	scanf("%d",&year);
-	if(year%200==0)
+	if(year%100==0)
 	{
-		if (year%500==0)
+		if (year%400==0)
 		printf("%d is leap year",year);
 		else
 		printf("%d is not leap year",year);
 	}
-	else if (year%5==0)
+	else if (year%4==0)
 	printf("%d is leap year",year);
 	else
 	printf("%d is not leap year",year);
